b1b1a2894b4a414764a5832dd92956a28d7d51dc
adnankhayrou <adnanekhayrou3@gmail.com>
2023-03-07 17:16:15 +0100

    adding sidebar and table of product to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,121 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-               
+                <div class="container-fluid">
+                    <div class="row flex-nowrap">
+                        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-light shadow">
+                            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                                    <span class="fs-5 d-none d-sm-inline mb-5"></span>
+                                </a>
+                                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link align-middle px-0">
+                                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
+                                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                                        <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
+                                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                                            <li class="w-100">
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+                                            </li>
+                                            <li>
+                                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="nav-link px-0 align-middle">
+                                            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                                    </li>
+                                </ul>
+                                <hr>
+                                
+                            </div>
+                        </div>
+                        <div class="col py-3">
+                            <div class="row items-center me-0">
+                                <h1 class="col fw-bold ms-5 mt-5">Your products</h1>
+                               
+                                <button class="col-4 me-5 mt-5 btn btn-primary w-auto" href="#modal-meal" data-bs-toggle="modal"><b>+ </b> Add Product</button>
+                                </div>
+                            <div class="container pt-5 table-responsive">
+                        
+                                        <table class="table table-responsive bg-white rounded">
+                                            <thead>
+                                              <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Image</th>
+                                                <th scope="col">Descriptio</th>
+                                                <th scope="col">Date</th>
+                                                <th scope="col">Edit</th>
+                                                <th scope="col">Delete</th>
+                                              </tr>
+                                            </thead>
+                                            <tbody>
+                                                @php
+                                                $count = 1;
+                                                @endphp
+                                            {{-- @foreach ($meals as $meal) --}}
+                                              <tr>
+                                                <td>test</td>
+                                                <td>test</td>
+                                                <td><img class="rounded" src="{{asset('/images/22.svg')}}" width="100" height="60"></td>
+                                                <td><span class="d-inline-block text-truncate" style="max-width: 150px;">test</span></td>
+                                                <td>test</td>
+                                                <td><a href="" class="text-decoration-none text-primary fw-bold">edit</a></td>
+                                                <td><a href="" class="text-decoration-none text-danger fw-bold">delete</a></td>
+                                              </tr>
+                                              
+                                               
+                                              @php
+                                                $count ++;
+                                                @endphp
+                                                {{-- @endforeach --}}
+                                            </tbody>
+                                         </table> 
+                                        
+                                   </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
